c9f3daf4f51d47baebc3521bb1fa027900aef5df
Phil Mitchell <pm1873@gmail.com>
2018-01-21 00:25:01 +0000

    Can add items to basket

--- src/main/java/Basket.java
+++ src/main/java/Basket.java
@@ -3,4 +3,16 @@ import java.util.ArrayList;
 public class Basket {
     ArrayList<Item> items;
 
+
+    public Basket() {
+        items = new ArrayList<>();
+    }
+
+    public int numberOfItemsInBasket() {
+        return this.items.size();
+    }
+
+    public void AddItemToBasket(Item item) {
+        this.items.add(item);
+    }
 }
--- src/test/java/BasketTest.java
+++ src/test/java/BasketTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class BasketTest {
+    Basket testBasket;
+    Item testItemOne;
+    Item testItemTwo;
+
+    @Before
+    public void before() {
+        testBasket = new Basket();
+        testItemOne = new Item("Burger");
+        testItemTwo = new Item("Cheese");
+    }
+
+    @Test
+    public void canAddItemToBasket() {
+        testBasket.AddItemToBasket(testItemOne);
+        testBasket.AddItemToBasket(testItemTwo);
+        assertEquals(2, testBasket.numberOfItemsInBasket());
+    }
 }
